9fdf7b8a1a141ab7e9a538ba38f0eff73ff7c76d
eid jabali <eidjabali@gmail.com>
2020-12-17 16:24:06 +0200

    make static folder and template folder

--- src/my_blog/settings.py
+++ src/my_blog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# if you want static folder to all project
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,"static"),
+    'C:/Users/abbas/Desktop/dev/Blogge/src/static',
+]
